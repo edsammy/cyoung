c3e19f881a07c38ade0e129dbb4af3d63e8f14ad
edsammy <eddiemsamuels@gmail.com>
2015-12-13 15:30:20 -0500

    fixed sequence issues

--- setup.sh
+++ setup.sh
@@ -6,10 +6,8 @@ mkdir motionPlayer && cd motionPlayer
 mkdir videos
 echo "downloading VLC media player..."
 curl -O http://mirror.de.leaseweb.net/videolan/vlc/2.2.1/macosx/vlc-2.2.1.dmg && \
-echo "moving VLC into Applications folder..."
-cd /Volumes && \
-hdiutil attach vlc-2.2.1.dmg && \
-cd vlc-2.2.1 && \
+hdiutil attach -quiet vlc-2.2.1.dmg && \
+cd /Volumes/vlc-2.2.1 && \
 cp -R VLC.app Applications
 cd ~/motionPlayer
 echo "editing VLC settings file..."
@@ -18,6 +16,8 @@ cp vlrc ~/Library/Preferences/org.videolan.vlc
 echo "downloading motionPlayer.app"
 curl -O https://raw.githubusercontent.com/edsammy/cyoung/master/motionPlayer.app
 echo "cleaning up..."
+cd /Volumes
 hdiutil detach vlc-2.2.1 && \
+cd ~/motionPlayer && \
 rm vlc-2.2.1.dmg
 echo "complete!"
