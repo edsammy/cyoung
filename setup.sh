4c74cad6ad2df87cd68807e8f3708125f8732313
edsammy <eddiemsamuels@gmail.com>
2015-12-13 15:38:28 -0500

    silenced some curl downloads

--- setup.sh
+++ setup.sh
@@ -11,10 +11,10 @@ cd /Volumes/vlc-2.2.1 && \
 cp -R VLC.app Applications
 cd ~/motionPlayer
 echo "editing VLC settings file..."
-curl -O https://raw.githubusercontent.com/edsammy/cyoung/master/vlrc && \
+curl -O --silent https://raw.githubusercontent.com/edsammy/cyoung/master/vlrc && \
 cp vlrc ~/Library/Preferences/org.videolan.vlc
 echo "downloading motionPlayer.app"
-curl -O https://raw.githubusercontent.com/edsammy/cyoung/master/motionPlayer.app
+curl -O --silent https://raw.githubusercontent.com/edsammy/cyoung/master/motionPlayer.app
 echo "cleaning up..."
 cd /Volumes
 hdiutil detach vlc-2.2.1 && \
